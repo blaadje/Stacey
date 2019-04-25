447fe9013934ee9a12f5a4ef32fd7cdad4ea0ff6
Alexandre Charlot <acharlot91@gmail.com>
2019-04-26 00:46:30 +0200

    adding build folder

---
+++ build/precache-manifest.85644c6a4eaab3e610b0d24170ed1524.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c3098e6e588ca1804320ab1ca73b5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95121f119ec7ce9c7e90",
+    "url": "/static/css/main.798b7763.chunk.css"
+  },
+  {
+    "revision": "5db3d3390aae7d2e8034",
+    "url": "/static/js/2.ee793906.chunk.js"
+  },
+  {
+    "revision": "95121f119ec7ce9c7e90",
+    "url": "/static/js/main.60960333.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import store from './store'
 
 import './index.css';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('stacey'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
